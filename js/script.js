7d1af5652e42ae9f8c40f0b7b8dfec356e3e350f
Aya Elnagar <aya_elnagar96@yahoo.com>
2022-11-05 21:20:38 +0200

    Add function to submit a number rating

--- js/script.js
+++ js/script.js
@@ -15,4 +15,23 @@ function selectNumberRating() {
     });
 }
 
+function submitNumberRating(value) {
+    submit.addEventListener('click', () => {
+        ratingContainer.classList.add('hide');
+        main.innerHTML = `
+        <!-- Thank you state start -->
+        <section class="thank-you-container">
+          <div class="image"><img src="images/illustration-thank-you.svg" alt="thank you image"></div>
+          <p class="user-select">You selected ${value} out of 5</p>
+          <h2>Thank you!</h2
+          <p>
+            We appreciate you taking the time to give a rating. If you ever need more support, 
+            don’t hesitate to get in touch!
+          </p>
+        </section>
+        <!-- Thank you state end -->
+        `;
+    })
+}
+
 selectNumberRating();
